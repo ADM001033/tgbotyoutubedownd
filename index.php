560c3d69486007585486e0441258f953494a06ac
ADM001033 <72941018+ADM001033@users.noreply.github.com>
2020-10-17 18:09:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'libs/Youtube.php';
 use Smoqadam\Telegram;
 use Smoqadam\Youtube;
 
-$api_token = 'API_TOKEN';
+$api_token = '1173660311:AAFGrcJdp6SP0Cf-r9e1Up3WE_o2AyEf6DQ';
 
 $tg = new Telegram($api_token);
 $y = new Youtube();
